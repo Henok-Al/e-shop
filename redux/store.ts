c1632df5be633196ab266eddcbb34c6e16caf55e
chernetGit <henok.gebresenbet@gmail.com>
2024-03-26 10:36:33 +0200

    product reducer

--- redux/store.ts
+++ redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cartSlices";
+import productReducer from "./features/productSlice";
 
 export const store = configureStore({
   reducer: {
     cartReducer,
+    productReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });

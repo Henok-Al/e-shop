261aa09b370bee58e2ed67e0a635cf7599242180
chernetGit <henok.gebresenbet@gmail.com>
2024-03-26 15:02:04 +0200

    connect to mongodb, fetch products

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ProductRow from "@/components/admin-panel/ProductRow";
 import { setLoading } from "@/redux/features/loadingSlice";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
@@ -31,7 +32,38 @@ const Dashbard = () => {
       .finally(() => dispatch(setLoading(false)));
   }, [updateTable]);
 
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <div className="bg-white h-calc(100vh-96px)] rounded-lg p-4">
+        <h2 className="text-3xl">All Products</h2>
+
+        <div className="mt-4 h-[calc(100vh-180px)] overflow-y-auto">
+          <table className="w-full">
+            <thead>
+              <tr className="text-gray-500 border-t border-[#ececec]">
+                <th>SR No.</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Picture</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product: IProduct, index) => (
+                <ProductRow
+                  key={product._id}
+                  srNo={index + 1}
+                  setOpenPopup={setOpenPopup}
+                  setUpdateTable={setUpdateTable}
+                  product={product}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashbard;

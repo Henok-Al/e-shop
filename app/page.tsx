d451e07c312be0e7d5618acba697aac2ac641f19
Henok-Al <henok.gebresenbet@gmail.com>
2024-04-05 01:22:26 +0300

    cart added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-"use client"
+"use client";
+import Cart from "@/components/front-end/Cart";
 import Navbar from "@/components/front-end/Navbar";
 import { useState } from "react";
 
 export default function Home() {
-  const [showCart, setShowCart] = useState();
+  const [showCart, setShowCart] = useState(false);
   return (
     <main>
-     <Navbar setShowCart={setShowCart}/>
-     
+      <Navbar setShowCart={setShowCart} />
+      {showCart && <Cart setShowCart={setShowCart} />}
     </main>
   );
 }
